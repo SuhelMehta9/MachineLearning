bfd50412815400a820338831abb82dd0df5fbe26
SuhelMehta9 <41117419+SuhelMehta9@users.noreply.github.com>
2019-03-28 00:26:42 +0530

    Add files via upload
    This is a support vector regression(SVR) applied on the data used in polynomial regression.

---
+++ SVR/svr_practice.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importing dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1:].values
+
+# Scaling the data
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X,y)
+
+# Visulising SVR
+plt.scatter(X,y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('SVR ploting')
+plt.xlabel('Position')
+plt.ylabel('Salary')
